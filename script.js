647f57f35c37aa855cb7bfd215e1bde5f4163902
Mohammad Qasimi <qasimimohammad819@gmail.com>
2025-08-13 14:19:28 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,10 +5,14 @@ const swiper = new Swiper('.mysiwper', {
   slidesPerView: 1,
   spaceBetween: 10,
 
- pagination:{
-    el: '.swiper-pagination',
-    clickable: true,
- },
+
+ 
+
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 
  breakpoints: {
      550: {
@@ -32,7 +36,36 @@ const swiper = new Swiper('.mysiwper', {
       spaceBetween: 50,
     },
   },
-  
 
+});
+// bestseller swiper
+
+// item swiper
+const bestseller = new Swiper('.bestseller-slider', {
+  // Optional parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+   // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+ breakpoints: {
+     550: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    },
+    1200 : {
+      slidesPerView: 4,
+    },
+  },
 
 });
